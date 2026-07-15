ac6323278696d622a923c296d8c02dfb0ad9513e
ManrajSingh02 <singhmanraj6702@gmail.com>
2026-07-15 15:18:10 +0530

    fetch data

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -3,9 +3,10 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'm32es5ry',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
+    appId: 'ybfh4cgsuwwvw1oxioqd9ngl',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
